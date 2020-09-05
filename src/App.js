af34b00158817c478b97414e0b89965b5867a695
RaidRafif <56295306+RaidRafif@users.noreply.github.com>
2020-09-05 14:50:59 +0700

    Adding feature

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
-export default function App() {
+const Profile = (props) => {
+  const [isFollowed, setIsFollowed] = useState(false);
+
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <React.Fragment>
+      <img src="https://via.placeholder.com/150" alt="profile picture" />
+      <div>@{props.username}</div>
+      <button onClick={() => setIsFollowed(!isFollowed)}>
+        {isFollowed ? "Unfollow" : "Follow"}
+      </button>
+      <div>Posts</div>
+      <div>Follower</div>
+      <div>Following</div>
+      <div>Bio</div>
+      <img src="https://via.placeholder.com/300" alt="profile picture" />
+    </React.Fragment>
   );
+};
+
+export default function App() {
+  const username = "instagram";
+  return <Profile username={username} />;
 }
